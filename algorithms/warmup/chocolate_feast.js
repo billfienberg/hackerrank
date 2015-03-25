3176aa3d7cc414986c216b70a0c8920be02ec726
Bill Fienberg <billfienberg@gmail.com>
2015-03-24 21:55:46 -0500

    Update chocolate_feast.js to adhere to the uShip JavaScript styleguide

--- algorithms/warmup/chocolate_feast.js
+++ algorithms/warmup/chocolate_feast.js
@@ -1,28 +1,30 @@
 function processData(input) {
     //Enter your code here
-var splitInput = input.split("\n")
-// console.log(splitInput)
+    var splitInput = input.split('\n');
+    // console.log(splitInput);
 
-for (var i = 0; i < splitInput.length; i++) {
-    splitInput[i] = splitInput[i].split(" ")
-    // console.log(splitInput[i])
-}
+    var buyChocolates = function buyChocolates (money, price, requirement) {
+        var money = money,
+            cost = price,
+            extra = requirement,
+            purchase = Math.floor(money/cost),
+            result = Math.floor(purchase + (purchase-1)/(requirement-1));
 
-var buyChocolates = function(money, price, requirement) {
-    var money = money
-    var cost = price
-    var extra = requirement
-    var purchase = Math.floor(money/cost)
-    var result = Math.floor(purchase + (purchase-1)/(requirement-1))
-    console.log(result)
-}
+        console.log(result);
+    };
 
-for (var i = 1; i < splitInput.length; i++) {
-  // console.log("splitInput[" + i + "]: " + splitInput[i])
-  buyChocolates(splitInput[i][0],splitInput[i][1],splitInput[i][2])
-  // console.log("----------------------------------------------------------------------------")
-}
-}
+    for (var i = 0; i < splitInput.length; i++) {
+        splitInput[i] = splitInput[i].split(' ');
+        // console.log(splitInput[i]);
+    };
+
+
+    for (var i = 1; i < splitInput.length; i++) {
+      // console.log('splitInput[' + i + ']: ' + splitInput[i]);
+      buyChocolates(splitInput[i][0],splitInput[i][1],splitInput[i][2]);
+      // console.log('----------------------------------------------------------------------------');
+    };
+};
 
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
