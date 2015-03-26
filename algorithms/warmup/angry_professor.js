24e7020b9c85e4dd989ee1cc5282dbdc30ba7c9b
Bill Fienberg <billfienberg@gmail.com>
2015-03-25 22:10:07 -0500

    Update angry_professor.js to better adhere to the uShip JavaScript styleguide

--- algorithms/warmup/angry_professor.js
+++ algorithms/warmup/angry_professor.js
@@ -7,25 +7,25 @@ function processData(input) {
 
     // Create a variable named splitInput
     // Split the input variable on the '\n' character
-    var splitInput = input.split("\n")
+    var splitInput = input.split("\n",
 
     // Log the result of the splutInput variable
     // console.log(splitInput)
 
     // Create a variable named numberOfTestCases
-    var numberOfTestCases;
+        numberOfTestCases;
 
     // Create a function named countNumberOfPunctualStudents that takes an array as a parameter
-    var countNumberOfPunctualStudents = function(array) {
+    var countNumberOfPunctualStudents = function countNumberOfPunctualStudents (array) {
 
         // Create a count variable and set it equal to 0
-        var count = 0
+        var count = 0;
 
         // Iterate through the array paramter
         for (var j = 0; j < array.length; j++) {
             if (array[j] <= 0) {
                 // If the index of the array is non-positive, increment the count variable by 1
-                count += 1
+                count += 1;
             }
         }
 
@@ -33,55 +33,55 @@ function processData(input) {
         // console.log("count: " + count)
 
         // Return the count variable
-        return count
+        return count;
     }
 
     // Iterate through the splitInput array
     for (var i = 0; i < splitInput.length; i++) {
 
         // Log the current index of the splitInput array
-        // console.log(splitInput[i])
+        // console.log(splitInput[i]);
 
         // Set the current index of the splitInput array eqaul to the result of splitting itself on the " " character
-        splitInput[i] = splitInput[i].split(" ")
+        splitInput[i] = splitInput[i].split(" ");
 
         // Log the current index of the splitInput array
-        // console.log("splitInput[" + i + "]: " + splitInput[i])
+        // console.log("splitInput[" + i + "]: " + splitInput[i]);
 
         if (i === 0) {
 
             // If the first index of the splitInput array equals 1, set the numberOfTestCases variable equal to the value at the index
-            numberOfTestCases = splitInput[i]
+            numberOfTestCases = splitInput[i];
 
             // Log the result of the numberOfTestCases variable
-            // console.log("numberOfTestCases: " + numberOfTestCases)
+            // console.log("numberOfTestCases: " + numberOfTestCases);
 
         } else if (i % 2 !== 0) {
 
             // If the current index of the splitInput array is odd
             // Create a variable named requisiteNumberOfPunctualStudents that is equal to the second index of the current index of the splitInput variable
-            var requisiteNumberOfPunctualStudents = splitInput[i][1]
+            var requisiteNumberOfPunctualStudents = splitInput[i][1];
 
             // Log the value of the requisiteNumberOfPunctualStudents variable
-            // console.log("requisiteNumberOfPunctualStudents: " + requisiteNumberOfPunctualStudents)
+            // console.log("requisiteNumberOfPunctualStudents: " + requisiteNumberOfPunctualStudents);
 
         } else if (i % 2 === 0) {
 
             // If the current index of the splitInput array is even
             // Create a count variable
             // Set it equal to the result of calling the countNumberOfPunctualStudents function with the current index of the splitInput array as an argument
-            var count = countNumberOfPunctualStudents(splitInput[i])
+            var count = countNumberOfPunctualStudents(splitInput[i]);
 
             if (count >= requisiteNumberOfPunctualStudents) {
                 // If the value of the count variable is greater than or equal to the value of the requisiteNumberOfPunctualStudents variable, then class is not cancelled
                 // Log the word, "NO"
-                console.log("NO")
+                console.log("NO");
 
             } else if (count < requisiteNumberOfPunctualStudents) {
 
                 // If the value of the count variable is less than the value of the requisiteNumberOfPunctualStudents variable, then class is cancelled
                 // Log the word, "YES"
-                console.log("YES")
+                console.log("YES");
 
             }
         }
