5ea5bf07121e52a79c24bddc805b74742d144594
Bill Fienberg <billfienberg@gmail.com>
2015-03-29 20:52:03 -0500

    Update cut_the_sticks.js to better adhere to the uShip JavaScript Styleguide

--- algorithms/warmup/cut_the_sticks.js
+++ algorithms/warmup/cut_the_sticks.js
@@ -3,10 +3,10 @@ function processData(input) {
     // var input = '6\n5 4 4 2 2 8';
     // console.log(input);
 
-    var splitInput = input.split('\n')
+    var splitInput = input.split('\n'),
+    numberOfSticks = splitInput.splice(0,1);
     // console.log(splitInput);
 
-    var numberOfSticks = splitInput.splice(0,1);
     // console.log(numberOfSticks[0]);
     // console.log(splitInput);
 
@@ -14,57 +14,56 @@ function processData(input) {
     // console.log(splitInput);
 
 
-    // console.log(lowest)
+    // console.log(lowest);
 
     var app = function app (array) {
-        var lowest;
-        var max;
-        // console.log(array)
+        var lowest, max;
+        // console.log(array);
 
       var findLowestNumberInArray = function findLowestNumberInArray (array) {
           for (var i = 0; i < array.length; i++) {
-              // console.log(array.length)
-              // console.log("array[i]: " + array[i])
-              array[i] = parseInt(array[i],10)
+              // console.log(array.length);
+              // console.log("array[i]: " + array[i]);
+              array[i] = parseInt(array[i],10);
               if (lowest == undefined) {
-                  lowest = array[i]
-                  // console.log("lowest: " + lowest)
+                  lowest = array[i];
+                  // console.log("lowest: " + lowest);
               } else if (array[i] <= lowest) {
-                  lowest = array[i]
-                  // console.log("lowest: " + lowest)
+                  lowest = array[i];
+                  // console.log("lowest: " + lowest);
               }
           }
       }
 
       var removeOrReduce = function removeOrReduce (array) {
-          // console.log("array.length: " + array.length)
-          console.log(array.length)
-          // console.log("-----------------------------------------------------------------------")
-          // console.log("array.length: " + array.length)
-          // console.log("lowest: " + lowest)
+          // console.log("array.length: " + array.length);
+          console.log(array.length);
+          // console.log("-----------------------------------------------------------------------");
+          // console.log("array.length: " + array.length);
+          // console.log("lowest: " + lowest);
           for (var j = 0; j < array.length; j++) {
               if (array[j] - lowest <= 0) {
-                  // console.log(array)
-                  array.splice(j,1)
-                  j -= 1
-                  // console.log(array)
+                  // console.log(array);
+                  array.splice(j,1);
+                  j -= 1;
+                  // console.log(array);
               } else if (array[j] - lowest > 0) {
-                  array[j] = array[j] - lowest
+                  array[j] = array[j] - lowest;
               }
           }
-          // console.log(array.length)
+          // console.log(array.length);
       }
 
       findLowestNumberInArray(array);
-      removeOrReduce(array)
+      removeOrReduce(array);
 
       if (array.length) {
-          app(array)
+          app(array);
       }
 
     }
 
-    app(splitInput)
+    app(splitInput);
 }
 
 process.stdin.resume();
